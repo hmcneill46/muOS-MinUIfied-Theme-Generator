4fc0e206b93cb76945f099c3d15a2ac78756b603
Harry McNeill <harryolivermcneill@gmail.com>
2024-11-12 13:35:10 +0000

    feat: make console name finder script make a fake ROMS folder for testing

--- Helper Scripts/CompareConsoleNameToLogos/main.py
+++ Helper Scripts/CompareConsoleNameToLogos/main.py
@@ -1,16 +1,13 @@
 import requests
 import os
+import time
+import shutil
 try:
     from githubToken import GITHUB_TOKEN
 except ImportError:
     raise ImportError("Please create a githubToken.py file with your GITHUB_TOKEN.")
 
-GITHUB_USER = "MustardOS"
-REPO_NAME = "internal"
-BRANCH = "main"
-FOLDER_PATH = "init/MUOS/info/assign"
-
-def get_console_names_from_github():
+def get_console_names_from_github(GITHUB_USER, REPO_NAME, BRANCH, FOLDER_PATH):
     # GitHub API URL to get the contents of the folder
     url = f"https://api.github.com/repos/{GITHUB_USER}/{REPO_NAME}/contents/{FOLDER_PATH}?ref={BRANCH}"
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
@@ -54,9 +51,22 @@ def get_console_names_from_github():
         return console_names
     else:
         raise Exception(f"Failed to retrieve data: {response.status_code} - {response.text}")
+    
+def FillRomsFolder(consoles, base_path):
+    for console in consoles:
+        console_path = os.path.join(base_path, console)
+        os.makedirs(console_path, exist_ok=True)
+        # add test file to each console folder
+        with open(os.path.join(console_path, 'content.txt'), 'w') as f:
+            f.write('content')
 
 def main():
-    console_names = get_console_names_from_github()
+    GITHUB_USER = "MustardOS"
+    REPO_NAME = "internal"
+    BRANCH = "main"
+    FOLDER_PATH = "init/MUOS/info/assign"
+
+    console_names = get_console_names_from_github(GITHUB_USER, REPO_NAME, BRANCH, FOLDER_PATH)
     directory_path = os.path.join("Assets", "System Logos", "png [5x]")
     console_name_status = {}
 
@@ -64,6 +74,11 @@ def main():
     color_found = "\033[92m"  # Green
     color_missing = "\033[91m"  # Red
     reset_color = "\033[0m"  # Reset to default
+
+    base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ROMS')
+    if os.path.exists(base_path):
+        shutil.rmtree(base_path)
+    FillRomsFolder(console_names, base_path)
 
     # Check the existence of each console's logo
     for console in console_names:
