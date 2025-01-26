e87a207339aa8a278538df5feee4da2aab7312a9
David Young <dmyoung9@asu.edu>
2025-01-25 20:18:07 -0800

    Refactor some settings handling for consistency between data types

--- generator/settings.py
+++ generator/settings.py
@@ -45,6 +45,7 @@ class SettingsManager:
         self.deviceScreenWidthVar = 640
 
         self.sections = []
+        self.default_values: dict[str, Any] = {}
         self.user_values: dict[str, Any] = {}
         self.merged_values: dict[str, Any] = {}
 
@@ -67,11 +68,20 @@ class SettingsManager:
                     user_val if user_val is not None else default_val, var_type
                 )
                 merged[var_name] = final_val
+                self.default_values[var_name] = default_val
 
                 if var_name == "device_type_var":
                     self._parse_screen_dimensions(final_val)
 
         self.merged_values = merged
+
+    def _get_var_type(self, var_name: str) -> str:
+        for section in self.sections:
+            for field in section.get("fields", []):
+                if field.get("var_name") == var_name:
+                    return field.get("var_type", "string")
+
+        return "string"
 
     def load(self):
         ensure_file_exists(self.base_path, {"sections": []})
@@ -105,7 +115,13 @@ class SettingsManager:
             encoding="utf-8",
         )
 
-    def set_value(self, var_name: str, new_value):
+    def set_value(self, var_name: str, new_value: Any) -> None:
+        default_value = self.default_values.get(var_name, None)
+        old_value = self.merged_values.get(var_name, None)
+        var_type = self._get_var_type(var_name)
+
+        new_value = self._parse_value(new_value, old_value, default_value, var_type)
+
         self.user_values[var_name] = new_value
         self.merged_values[var_name] = new_value
 
@@ -132,24 +148,109 @@ class SettingsManager:
                 "Invalid device type format, cannot find screen dimensions"
             )
 
+    def _parse_value(
+        self,
+        new_value: Any,
+        old_value: Any | None = None,
+        default_value: Any | None = None,
+        var_type: str = "string",
+    ) -> Any:
+        parsed_value = new_value
+
+        if var_type == "color":
+            parsed_value = self._parse_color_hex(new_value, old_value, default_value)
+        if var_type == "int":
+            parsed_value = self._parse_integer(new_value, old_value, default_value)
+        if var_type == "path":
+            parsed_value = self._parse_path(new_value, old_value, default_value)
+
+        return parsed_value
+
+    def _parse_color_hex(
+        self,
+        color_hex: str,
+        old_color_hex: str | None = None,
+        default_color_hex: str | None = "#000000",
+    ) -> str:
+        hex_pattern = r"^#?([0-9a-f]{6}){1}$"
+
+        if color_match := re.fullmatch(hex_pattern, color_hex, re.IGNORECASE):
+            return f"#{color_match.group(1)}".lower()
+        elif old_color_hex and (
+            old_color_match := re.fullmatch(hex_pattern, old_color_hex, re.IGNORECASE)
+        ):
+            return f"#{old_color_match.group(1)}".lower()
+        elif default_color_match := re.fullmatch(
+            hex_pattern, str(default_color_hex), re.IGNORECASE
+        ):
+            return f"#{default_color_match.group(1)}".lower()
+
+        return "#000000"
+
+    def _parse_integer(
+        self,
+        value: str | int,
+        old_value: str | int | None = None,
+        default_value: str | int | None = 0,
+    ) -> int:
+        if value and str(value).isdigit():
+            return int(value)
+        elif old_value and str(old_value).isdigit():
+            return int(old_value)
+        elif str(default_value).isdigit():
+            return int(default_value) if default_value is not None else 0
+
+        return 0
+
+    def _parse_path(
+        self,
+        value: str | Path,
+        old_value: str | Path | None = None,
+        default_value: str | Path | None = None,
+    ) -> Path | None:
+        if isinstance(value, str):
+            if value and (path := Path(value)).exists():
+                return path.resolve()
+            else:
+                return None
+        elif (
+            isinstance(old_value, Path)
+            and old_value
+            and (path := Path(old_value)).exists()
+        ):
+            return path.resolve()
+        elif (
+            isinstance(default_value, Path)
+            and default_value
+            and (path := Path(default_value)).exists()
+        ):
+            return path.resolve()
+
+        return None
+
     def get_sections(self):
         return self.sections
 
-    def __getattr__(self, name):
-        return self.get_value(name, None)
+    def __setattr__(self, name: str, value: Any):
+        self.set_value(name, value)
+
+    def __getattr__(self, name: str) -> Any:
+        return self.get_value(name)
 
     def _cast_from_json(self, value: Any, var_type: str) -> Any:
         if value is None:
             return None
 
         if var_type == "int":
-            return int(value)
+            return self._parse_integer(value)
         elif var_type == "float":
             return float(value)
         elif var_type == "boolean":
             return bool(value)
         elif var_type == "path":
-            return Path(value)
+            return parsed_path if (parsed_path := self._parse_path(value)) else None
+        elif var_type == "color":
+            return self._parse_color_hex(value)
         else:
             return str(value)
 
@@ -158,12 +259,14 @@ class SettingsManager:
             return None
 
         if var_type == "int":
-            return int(value)
+            return self._parse_integer(value)
         elif var_type == "float":
             return float(value)
         elif var_type == "boolean":
             return bool(value)
         elif var_type == "path":
-            return str(Path(value).resolve())
+            return str(parsed_path) if (parsed_path := self._parse_path(value)) else ""
+        elif var_type == "color":
+            return self._parse_color_hex(value)
         else:
             return str(value)
