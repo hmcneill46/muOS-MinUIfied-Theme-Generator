cceb51af4d92eeba19cb82fbc2565bb0f0250bf7
David Young <dmyoung9@asu.edu>
2025-02-16 19:39:28 -0800

    Add vertical layout preview

--- src/muos_minuify/generator/preview.py
+++ src/muos_minuify/generator/preview.py
@@ -1,9 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from PIL.Image import Resampling
 
 from . import ThemeGenerator
 from .components.header.preview import PreviewHeaderBubbles
 from ..settings import SettingsManager
+from ..font import get_font_path
 
 
 class ThemePreviewGenerator(ThemeGenerator):
@@ -76,6 +77,129 @@ class ThemePreviewGenerator(ThemeGenerator):
         )
         return header_bubbles_image
 
+    def _generate_launcher_list(
+        self,
+        draw: ImageDraw.ImageDraw,
+        selected_item: str = "explore",
+    ) -> None:
+        item_map = {
+            "explore": "Content Explorer",
+            "favourite": "Favourites",
+            "history": "History",
+            "apps": "Utilities",
+            "info": "Information",
+            "config": "Configuration",
+            "reboot": "Reboot",
+            "shutdown": "Shutdown",
+        }
+
+        text_padding = int(self.manager.textPaddingVar * self.render_factor)
+        bubble_padding = int(self.manager.bubblePaddingVar * self.render_factor)
+        screen_width = int(self.manager.deviceScreenWidthVar * self.render_factor)
+        screen_height = int(self.manager.deviceScreenHeightVar * self.render_factor)
+
+        items_per_screen = int(self.manager.itemsPerScreenVar)
+
+        text_alignment = self.manager.global_alignment_var
+        content_padding_top = int(
+            self.manager.contentPaddingTopVar * self.render_factor
+        )
+        approx_footer_height = int(
+            self.manager.approxFooterHeightVar * self.render_factor
+        )
+        individual_item_height = round(
+            (screen_height - approx_footer_height - content_padding_top)
+            / items_per_screen
+        )
+        max_boxart_width = int(self.manager.maxBoxArtWidth * self.render_factor)
+        max_bubble_length = screen_width - max_boxart_width
+
+        selected_font_hex = self.manager.selectedFontHexVar
+        deselected_font_hex = self.manager.deselectedFontHexVar
+        bubble_hex = self.manager.bubbleHexVar
+
+        font_size = int(self.manager.font_size_var * self.render_factor)
+
+        selected_font_path = get_font_path(
+            self.manager.use_alt_font_var, self.manager.alt_font_filename
+        )
+        font = ImageFont.truetype(selected_font_path, font_size)
+
+        smallestValidText_bbox = font.getbbox("_...")
+        smallestValidTest_width = smallestValidText_bbox[2] - smallestValidText_bbox[0]
+
+        for index, (app, label) in enumerate(item_map.items()):
+            if index >= items_per_screen:
+                break
+
+            num_letters_cut = 0
+            text_width = float("inf")
+            text = label
+            text_color = deselected_font_hex
+
+            if (
+                max_bubble_length
+                < text_padding
+                + smallestValidTest_width
+                + bubble_padding
+                + 5 * self.render_factor
+            ):  # Make sure there won't be a bubble error
+                max_bubble_length = screen_width
+
+            if app == selected_item:
+                text_color = selected_font_hex
+
+                total_current_length = text_padding + text_width + bubble_padding
+            else:
+                total_current_length = text_padding + text_width
+
+            while total_current_length > max_bubble_length:
+                if num_letters_cut > 0:
+                    text = label[: -(num_letters_cut + 3)]
+                    text = label + "..."
+
+                text_bbox = font.getbbox(label)
+                text_width = text_bbox[2] - text_bbox[0]
+                if app == selected_item:
+                    total_current_length = text_padding + text_width + bubble_padding
+                else:
+                    total_current_length = text_padding + text_width
+
+                num_letters_cut += 1
+                if text == "...":
+                    raise ValueError(
+                        "'Cut bubble off at' too low\n\nPlease use a different custom 'cut bubble off' at value"
+                    )
+
+            text_x = 0
+            if text_alignment == "Left":
+                text_x = text_padding
+            elif text_alignment == "Right":
+                text_x = screen_width - text_padding - text_width
+            elif text_alignment == "Centre":
+                text_x = (screen_width - text_width) / 2
+
+            rectangle_x0 = text_x - bubble_padding
+            rectangle_y0 = content_padding_top + individual_item_height * index
+            rectangle_x1 = rectangle_x0 + bubble_padding + text_width + bubble_padding
+            rectangle_y1 = content_padding_top + individual_item_height * (index + 1)
+            middle_y = (rectangle_y0 + rectangle_y1) / 2
+            ascent, descent = font.getmetrics()
+            text_height = ascent + descent
+
+            # Calculate the text's y-position by centreing it vertically within the rectangle
+            text_y = middle_y - (text_height / 2)
+
+            corner_radius = individual_item_height // 2
+
+            if app == selected_item:
+                draw.rounded_rectangle(
+                    [(rectangle_x0, rectangle_y0), (rectangle_x1, rectangle_y1)],
+                    radius=corner_radius,
+                    fill=bubble_hex,
+                )
+            draw.text((text_x, text_y), text, font=font, fill=text_color)
+
     def generate_wall_image(
         self,
         right_buttons: list[tuple[str, str]] = [],
@@ -116,13 +240,14 @@ class ThemePreviewGenerator(ThemeGenerator):
         left_buttons: list[tuple[str, str]] = [("POWER", "SLEEP")],
         selected_item: str = "explore",
     ) -> Image.Image:
-        if (variant := self.manager.main_menu_style_var) == "Vertical":
-            return self.generate_static_image(right_buttons, left_buttons)
-
         image = self._generate_background()
 
-        launcher_icons_image = self._generate_launcher_icons(selected_item, variant)
-        image.alpha_composite(launcher_icons_image)
+        if (variant := self.manager.main_menu_style_var) == "Vertical":
+            draw = ImageDraw.Draw(image)
+            self._generate_launcher_list(draw, selected_item)
+        else:
+            launcher_image = self._generate_launcher_icons(selected_item, variant)
+            image.alpha_composite(launcher_image)
 
         header_bubbles_image = self._generate_header_bubbles(selected_item)
         image.alpha_composite(header_bubbles_image)
